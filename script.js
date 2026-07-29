0ec28106a2f5dbad8bb47f55da9f6459f9c395bc
anaghamangalasseri-dot <anaghamangalasseri@gmail.com>
2026-07-29 18:59:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Portfolio Loaded");
+// Portfolio Loaded
+console.log("Portfolio Loaded Successfully!");
+
+// Smooth button animation
+const button = document.querySelector(".btn");
+
+button.addEventListener("mouseover", () => {
+    button.style.transform = "scale(1.05)";
+});
+
+button.addEventListener("mouseout", () => {
+    button.style.transform = "scale(1)";
+});
